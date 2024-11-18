f6fc45e88dbbb60ed17d995533ffdca9ec538ad2
Ben Manning <bmanning@me.com>
2024-11-18 12:15:53 -0500

    init commit

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  hashedPassword: String
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
